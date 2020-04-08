1b596af23360827fb0a4f92c3d4ebc08322eef4d
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-04-08 19:32:20 +0100

    feature(Signup):page for users to signup Page for users to signup with social accounts

---
+++ src/views/login/Login.js
@@ -0,0 +1,83 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import "./Login.css";
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
+
+const Login = (props) => {
+  return (
+    <section className="banner row">
+      <div className="col-md-8 perfect-center">
+        <h2 className="auth-logo  font-bold">ibloov LOGO</h2>
+      </div>
+      <div className="col-md-4 perfect-center">
+        <div className="auth-container">
+          <div>
+            <h4 className="font-bold">Sign in</h4>
+            <div>
+              <p className="text-muted small-info-text">
+                New User?{" "}
+                <Link to="/signup" className="font-bold text-dark">
+                  {" "}
+                  Sign up now!
+                </Link>
+              </p>
+            </div>
+          </div>
+          <form>
+            <Input
+              name="email"
+              type="email"
+              customClassName="form-control auth-input"
+              id="email"
+              placeHolder="Email address"
+              aria-describedby="emailHelp"
+              required
+              // value={this.state.email}
+              // handleChange={this.emailChange.bind(this)}
+            />
+            <div className="auth-button-container-login">
+              <Button
+                customClassName="auth-button bold-600"
+                // onclick={this.onButtonPress.bind(this)}
+              >
+                Continue
+              </Button>
+            </div>
+            <div className="row divider-container mt-5">
+              <hr className="col-md-3 col-sm-3 divider" />
+              <p className="mr-3 ml-3">Or</p>
+              <hr className="col-md-3 col-sm-3 divider" />
+            </div>
+            <div className="auth-button-container-social-login">
+              <Button
+                customClassName="btn-outline-secondary bold-600 auth-google mb-2"
+                // onclick={this.onButtonPress.bind(this)}
+              >
+                Continue with Google
+              </Button>
+              <Button
+                customClassName="auth-facebook bold-600 mb-2"
+                // onclick={this.onButtonPress.bind(this)}
+              >
+                Continue with Facebook
+              </Button>
+              <Button
+                customClassName="auth-twitter bold-600 mb-2"
+                // onclick={this.onButtonPress.bind(this)}
+              >
+                Continue with Twitter
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Login.propTypes = {};
+
+export default Login;
