12af47390ed22089622be7f15041e69349b9c7b3
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-04-14 11:28:05 +0100

    Updates

---
+++ precache-manifest.56a9018bd1aaaadfa3073edf9693f552.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a92b892cc82927e02ade8007914d65",
+    "url": "/ibloov-web/index.html"
+  },
+  {
+    "revision": "3d0756ef21d49334521a",
+    "url": "/ibloov-web/static/css/main.74551706.chunk.css"
+  },
+  {
+    "revision": "7a1e3dabde4fcd4898c1",
+    "url": "/ibloov-web/static/js/2.3e97cf1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ibloov-web/static/js/2.3e97cf1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0756ef21d49334521a",
+    "url": "/ibloov-web/static/js/main.fb89692a.chunk.js"
+  },
+  {
+    "revision": "bef2d0b440a6ac1c2e4e",
+    "url": "/ibloov-web/static/js/runtime-main.4ae8899f.js"
+  },
+  {
+    "revision": "f7b77993b45b200d7e017ab04fc722ac",
+    "url": "/ibloov-web/static/media/background.f7b77993.jpg"
+  },
+  {
+    "revision": "5b97ffe20018219ba61e3400999ae409",
+    "url": "/ibloov-web/static/media/bg2.5b97ffe2.jpg"
+  },
+  {
+    "revision": "6a7e7f74737414719faa2d229937280f",
+    "url": "/ibloov-web/static/media/bg3.6a7e7f74.jpg"
+  },
+  {
+    "revision": "e26b6767967e16684c8f0faca64eac4c",
+    "url": "/ibloov-web/static/media/passport.e26b6767.jpg"
+  }
+]);
