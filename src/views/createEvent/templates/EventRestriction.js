e0e0dc37fa3e9571016157b4634a41139bdbf6fe
Damilola Adekoya <dharmykoya38@gmail.com>
2020-10-11 23:36:43 +0100

    techFix(create-event):enahance the validation of  every step of the form

--- src/views/createEvent/templates/EventRestriction.js
+++ src/views/createEvent/templates/EventRestriction.js
@@ -13,7 +13,10 @@ const EventRestriction = props => {
     eventRestrictionsHandler,
     notificationHandler,
     addCollaborator,
-    addInvitee
+    addInvitee,
+       formCount,
+    submitEventHandler,
+    previousQuestionHandler
   } = props;
   const [womenOnly, setWomenOnly] = useState(false);
   const [childrenOnly, setChildrenOnly] = useState(false);
@@ -22,6 +25,7 @@ const EventRestriction = props => {
   const [none, setNone] = useState(false);
   const [noChildren, setNoChildren] = useState(false);
   const [openGiftWishList, setOpenGiftWishList] = useState(false);
+  const [selectedOpt, setSelectedOpt] = useState("");
 
   const openGiftwishListHandler = () => {
     setOpenGiftWishList(!openGiftWishList);
@@ -31,6 +35,7 @@ const EventRestriction = props => {
     e.preventDefault();
 
     eventRestrictionsHandler(id);
+    setSelectedOpt(id)
 
     switch (name) {
       case "womenOnly":
@@ -56,85 +61,87 @@ const EventRestriction = props => {
         return;
     }
   };
+let validateNextBtn = !selectedOpt ? true : false;
   return (
-    <div className="row">
-      <div>
-        <div className="create-event-title-header mb-3">
-          <h5>Any restriction to the event?</h5>
-          <small>Set conditions to be met before attending the event</small>
-        </div>
-        <div className="row restrict-options">
-          <RadioInput
-            radioName="Women Only"
-            customClass="form-check-input"
-            name="womenOnly"
-            labelName="womenOnly"
-            id="women Only"
-            selectedClass={womenOnly}
-            onSelectOption={selectOptionHandler}
-            checked={womenOnly}
-          />
-          <RadioInput
-            radioName="None"
-            customClass="form-check-input"
-            name="none"
-            labelName="none"
-            id="none"
-            selectedClass={none}
-            onSelectOption={selectOptionHandler}
-          />
-          <RadioInput
-            radioName="Senior Citizen"
-            customClass="form-check-input"
-            name="seniorCitizen"
-            labelName="seniorCitizen"
-            id="senior Citizen"
-            selectedClass={seniorCitizen}
-            onSelectOption={selectOptionHandler}
-          />
-          <RadioInput
-            radioName="Children Only"
-            customClass="form-check-input"
-            name="childrenOnly"
-            labelName="childrenOnly"
-            id="children Only"
-            selectedClass={childrenOnly}
-            onSelectOption={selectOptionHandler}
-          />
-          <RadioInput
-            radioName="No Children"
-            customClass="form-check-input"
-            name="noChildren"
-            labelName="noChildren"
-            id="no Children"
-            // checked={}
-            selectedClass={noChildren}
-            onSelectOption={selectOptionHandler}
-          />
-          <RadioInput
-            radioName="Event 18+"
-            customClass="form-check-input"
-            name="adult"
-            labelName="adult"
-            id="adult"
-            selectedClass={adult}
-            onSelectOption={selectOptionHandler}
-            // onSelectOption={selectOptionHandler}
-            // checkedClass={state.adult ? "restrict-selected" : ""}
-          />
-        </div>
+    <div>
+      <div className="row">
+        <div>
+          <div className="create-event-title-header mb-3">
+            <h5>Any restriction to the event?</h5>
+            <small>Set conditions to be met before attending the event</small>
+          </div>
+          <div className="row restrict-options">
+            <RadioInput
+              radioName="Women Only"
+              customClass="form-check-input"
+              name="womenOnly"
+              labelName="womenOnly"
+              id="women Only"
+              selectedClass={womenOnly}
+              onSelectOption={selectOptionHandler}
+              checked={womenOnly}
+            />
+            <RadioInput
+              radioName="None"
+              customClass="form-check-input"
+              name="none"
+              labelName="none"
+              id="none"
+              selectedClass={none}
+              onSelectOption={selectOptionHandler}
+            />
+            <RadioInput
+              radioName="Senior Citizen"
+              customClass="form-check-input"
+              name="seniorCitizen"
+              labelName="seniorCitizen"
+              id="senior Citizen"
+              selectedClass={seniorCitizen}
+              onSelectOption={selectOptionHandler}
+            />
+            <RadioInput
+              radioName="Children Only"
+              customClass="form-check-input"
+              name="childrenOnly"
+              labelName="childrenOnly"
+              id="children Only"
+              selectedClass={childrenOnly}
+              onSelectOption={selectOptionHandler}
+            />
+            <RadioInput
+              radioName="No Children"
+              customClass="form-check-input"
+              name="noChildren"
+              labelName="noChildren"
+              id="no Children"
+              // checked={}
+              selectedClass={noChildren}
+              onSelectOption={selectOptionHandler}
+            />
+            <RadioInput
+              radioName="Event 18+"
+              customClass="form-check-input"
+              name="adult"
+              labelName="adult"
+              id="adult"
+              selectedClass={adult}
+              onSelectOption={selectOptionHandler}
+              // onSelectOption={selectOptionHandler}
+              // checkedClass={state.adult ? "restrict-selected" : ""}
+            />
+          </div>
 
-        <div className="gift-wishlist mt-3">
-          <FontAwesomeIcon className="" icon="gift" />
-          <Button
-            customClassName="wishlist-button bold-600"
-            onClick={openGiftwishListHandler}
-            btndisabled={false}
-          >
-            Add Gift Wishlist
-          </Button>
-        </div>
-        {/* <div className="gift-wishlist mt-3">
+          <div className="gift-wishlist mt-3">
+            <FontAwesomeIcon className="" icon="gift" />
+            <Button
+              customClassName="wishlist-button bold-600"
+              onClick={openGiftwishListHandler}
+              btndisabled={false}
+            >
+              Add Gift Wishlist
+            </Button>
+          </div>
+          {/* <div className="gift-wishlist mt-3">
           <FontAwesomeIcon className="" icon="gift" />
           <Button
             customClassName=" wishlist-button bold-600"
@@ -144,24 +151,49 @@ const EventRestriction = props => {
             Add Events to "FUND ME"
           </Button>
         </div> */}
-        <div className="mt-4">
-          <p className="private-toggle">Notify me when people join </p>
-          <Toggle handleClick={notificationHandler} />
+          <div className="mt-4">
+            <p className="private-toggle">Notify me when people join </p>
+            <Toggle handleClick={notificationHandler} />
+          </div>
         </div>
+        <div>
+          <EventCollaborators
+            addCollaborator={addCollaborator}
+            addInvitee={addInvitee}
+          />
+        </div>
+        <SideOverLayContainer
+          openSide={openGiftWishList}
+          customClassName="wishlist-side-container"
+          toggleOpenSide={openGiftwishListHandler}
+        >
+          <GiftWishList />
+        </SideOverLayContainer>
       </div>
-      <div>
-        <EventCollaborators
-          addCollaborator={addCollaborator}
-          addInvitee={addInvitee}
-        />
-      </div>
-      <SideOverLayContainer
-        openSide={openGiftWishList}
-        customClassName="wishlist-side-container"
-        toggleOpenSide={openGiftwishListHandler}
+
+      <div
+        // className="myibloov-create-button-container"
+        className={
+          formCount === 2
+            ? "myibloov-create-button-container myibloov-description-btn"
+            : "myibloov-create-button-container"
+        }
       >
-        <GiftWishList />
-      </SideOverLayContainer>
+        <Button
+          customClassName="mybloov-create-event-btn-2  bold-600"
+          onClick={previousQuestionHandler}
+          btndisabled={false}
+        >
+          {formCount === 1 ? "Cancel" : "Back"}
+        </Button>
+        <Button
+          customClassName="mybloov-create-event-btn-2  bold-600"
+          onClick={submitEventHandler}
+          btndisabled={validateNextBtn}
+        >
+          {formCount === 4 ? "Create Event" : "Next"}
+        </Button>
+      </div>
     </div>
   );
 };
